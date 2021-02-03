257e762a0e1646b9b9d36150dd37f5c7ba3bb68f
DaisyBell2 <alina_borz@mail.ru>
2021-02-03 23:50:49 +0300

    received Data from the Network

--- app/src/main/java/com/daisybell/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/daisybell/cryptoapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.daisybell.cryptoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.daisybell.cryptoapp.api.ApiFactory
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val disposable  = ApiFactory.apiService.getFullPriceList(fSyms = "BTC,ETH")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("TEST_OF_LOADING_DATA", "1: $it")
+            }, {
+                Log.d("TEST_OF_LOADING_DATA", "2: ${it.message}")
+            })
+
+        compositeDisposable.add(disposable)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
+    }
+
 }
--- app/src/main/java/com/daisybell/cryptoapp/pojo/CoinPriceInfoRowData.kt
+++ app/src/main/java/com/daisybell/cryptoapp/pojo/CoinPriceInfoRowData.kt
@@ -1,12 +1,12 @@
 package com.daisybell.cryptoapp.pojo
 
+import com.google.gson.JsonObject
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
-import org.json.JSONObject
 
 
 data class CoinPriceInfoRowData(
     @SerializedName("RAW")
     @Expose
-    val coinPriceInfoJsonObject: JSONObject? = null
+    val coinPriceInfoJsonObject: JsonObject? = null
 )
