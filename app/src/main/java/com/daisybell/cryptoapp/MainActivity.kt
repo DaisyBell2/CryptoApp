1b1699b1b65ba5e0b18feb122d30b25c312fd40e
DaisyBell2 <alina_borz@mail.ru>
2021-02-04 15:37:47 +0300

    Add and configure viewModel

--- app/src/main/java/com/daisybell/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/daisybell/cryptoapp/MainActivity.kt
@@ -1,36 +1,26 @@
 package com.daisybell.cryptoapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.daisybell.cryptoapp.api.ApiFactory
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
 
-    private val compositeDisposable = CompositeDisposable()
+    private lateinit var viewModel: CoinViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val disposable  = ApiFactory.apiService.getFullPriceList(fSyms = "BTC,ETH")
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Log.d("TEST_OF_LOADING_DATA", "1: $it")
-            }, {
-                Log.d("TEST_OF_LOADING_DATA", "2: ${it.message}")
-            })
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        viewModel.loadData()
+        viewModel.priceList.observe(this, Observer {
+            Log.d("TEST_OF_LOADING_DATA", "Success in activity $it")
+        })
 
-        compositeDisposable.add(disposable)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        compositeDisposable.dispose()
-    }
 
 }
