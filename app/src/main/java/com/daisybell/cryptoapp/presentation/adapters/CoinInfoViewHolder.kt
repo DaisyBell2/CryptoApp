8b0311150cc1b68dcbed9a9337b3af2323cdc02d
DaisyB_ell <alina_borz@mail.ru>
2023-01-13 09:21:29 +0300

    Use view binding in adapter

---
+++ app/src/main/java/com/daisybell/cryptoapp/presentation/adapters/CoinInfoViewHolder.kt
@@ -0,0 +1,8 @@
+package com.daisybell.cryptoapp.presentation.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+import com.daisybell.cryptoapp.databinding.ItemCoinInfoBinding
+
+class CoinInfoViewHolder(
+    val binding: ItemCoinInfoBinding
+) : RecyclerView.ViewHolder(binding.root)
