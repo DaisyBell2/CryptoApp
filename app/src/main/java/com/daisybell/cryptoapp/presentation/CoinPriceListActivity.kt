49864279582b1f64a148422d6020fdb7cae27548
DaisyB_ell <alina_borz@mail.ru>
2023-01-14 08:54:34 +0300

    Use master/detail flow in main screen

--- app/src/main/java/com/daisybell/cryptoapp/presentation/CoinPriceListActivity.kt
+++ app/src/main/java/com/daisybell/cryptoapp/presentation/CoinPriceListActivity.kt
@@ -3,6 +3,7 @@ package com.daisybell.cryptoapp.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.daisybell.cryptoapp.R
 import com.daisybell.cryptoapp.databinding.ActivityCoinPriceListBinding
 import com.daisybell.cryptoapp.domain.CoinInfo
 import com.daisybell.cryptoapp.presentation.adapters.CoinInfoAdapter
@@ -22,11 +23,12 @@ class CoinPriceListActivity : AppCompatActivity() {
         val adapter = CoinInfoAdapter(this)
         adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
             override fun onCoinClick(coinInfoDto: CoinInfo) {
-                val intent = CoinDetailActivity.newIntent(
-                    this@CoinPriceListActivity,
-                    coinInfoDto.fromSymbol
-                )
-                startActivity(intent)
+                supportFragmentManager.popBackStack()
+                if (isOnePaneMode()) {
+                    launchDetailActivity(coinInfoDto.fromSymbol)
+                } else {
+                    launchDetailFragment(coinInfoDto.fromSymbol)
+                }
             }
         }
         binding.rvCoinPriceList.adapter = adapter
@@ -38,5 +40,23 @@ class CoinPriceListActivity : AppCompatActivity() {
 
     }
 
+    internal fun isOnePaneMode() = binding.fragmentContainer == null
+
+    internal fun launchDetailActivity(fromSymbol: String) {
+        val intent = CoinDetailActivity.newIntent(
+            this@CoinPriceListActivity,
+            fromSymbol
+        )
+        startActivity(intent)
+    }
+
+
+    internal fun launchDetailFragment(fromSymbol: String) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
+            .addToBackStack(null)
+            .commit()
+    }
 
 }
